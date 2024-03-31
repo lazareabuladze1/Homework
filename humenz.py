ea66acc33fddabbdd4a803eb2f45e4318ce40884
lazare <lazareabuladze7@gmail.com>
2024-03-31 09:47:50 +0400

    second commit

---
+++ humenz.py
@@ -0,0 +1,7 @@
+mother="natiko"
+father="vaxusti"
+sister="liziko"
+me="lazhare"
+grandmother="inga xatuna"
+gradfather="nugzari zaliko"
+print("natiko", "vaxusti", "liziko", "lazhare", "inga xatuna", "nugzari zaliko") 
